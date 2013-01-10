ae03c39feb5b584a6915468f5c448d69b5a2c7f8
madohata <4444ffffoota@gmail.com>
2013-01-10 16:14:27 +0900

    ミスがあったので修正・・

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.listen(3000);
 	 var INIT_CHIP = 100;
 	 
 	 // Socket.ioの準備
-	 var io = require('socket.io').listen(express.createServer().listen(app));
+	 var io = require('socket.io').listen(app);
 
 	/**
 	 * ユーザー情報の管理
